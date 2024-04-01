b0dd4ae377f8c71f58d681dc331d07b57eb6ee2a
cslian14 <casulian14@gmail.com>
2024-04-01 20:24:09 +0800

    Add to github

---
+++ CasulMyAppCRUD/CasulMyAppCRUD/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using CasulMyAppCRUD.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasulMyAppCRUD.Controllers
+{
+    public class BaseController : Controller
+    {
+        public CasulEntities _db;
+        public BaseRepository<User> _userRepo;
+        public BaseController()
+        {
+            _db = new CasulEntities();
+            _userRepo = new BaseRepository<User>();
+        }
+
+    }
+}
